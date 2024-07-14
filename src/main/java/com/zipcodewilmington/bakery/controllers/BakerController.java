c6486e4883b7f32eb692b9ec597a7468a74da67f
Danny Cao <dannycao1997@yahoo.com>
2024-07-14 14:33:52 -0400

    spring boot practice

--- src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
+++ src/main/java/com/zipcodewilmington/bakery/controllers/BakerController.java
@@ -4,30 +4,38 @@ import com.zipcodewilmington.bakery.models.Baker;
 import com.zipcodewilmington.bakery.services.BakerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+@RestController
 public class BakerController {
+
     private BakerService service;
 
     public BakerController(BakerService service) {
         this.service = service;
     }
 
+    @RequestMapping("/bakers") //GET ALL BAKERS
     public ResponseEntity<Iterable<Baker>> index() {
         return new ResponseEntity<>(service.index(), HttpStatus.OK);
     }
 
+    @RequestMapping("/baker/{id}") //GET SPECIFIC BAKER BY ID
     public ResponseEntity<Baker> show(Long id) {
         return new ResponseEntity<>(service.show(id), HttpStatus.OK);
     }
 
+    @PostMapping("/baker") //CREATE NEW BAKER
     public ResponseEntity<Baker> create(Baker baker) {
         return new ResponseEntity<>(service.create(baker), HttpStatus.CREATED);
     }
 
+    @PutMapping("/baker/{id}") //UPDATE SPECIFIC BAKER BY ID
     public ResponseEntity<Baker> update(Long id, Baker baker) {
         return new ResponseEntity<>(service.update(id, baker), HttpStatus.OK);
     }
 
+    @DeleteMapping("/baker/{id}") //DELETE SPECIFIC BAKER BY ID
     public ResponseEntity<Boolean> destroy(Long id) {
         return new ResponseEntity<>(service.delete(id), HttpStatus.OK);
     }
